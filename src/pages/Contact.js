57cd8ee53e4ae27daa9e46994c097bcc92ed854b
michaelc143 <mcorbishley@wisc.edu>
2023-01-02 17:42:32 -0600

    working with css

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -9,15 +9,15 @@ export default function Contact() {
             <form action='mailto:corbishleycollege@gmail.com' method='POST' encType='text/plain' name='EmailForm'>
             <label for="Name" className='name-input'>Your name</label>
             <br />
-            <input type="text" id="Name" name="Name" required></input>
+            <input type="text" id="Name" name="Name" required className='name-textbox'></input>
             <br />
             <label for="Email" className='email-input'>Your email address</label>
             <br />
-            <input type="email" id="Email" name="Email" required></input>
+            <input type="email" id="Email" name="Email" required className='email-textbox'></input>
             <br />
             <label for="Message" className='msg-input'>Your message</label>
             <br />
-            <textarea id="Message" name="Message" rows="6" maxlength="3000" required></textarea>
+            <textarea id="Message" name="Message" rows="6" maxlength="3000" required className='msg-textbox'></textarea>
             <br />
             <button type="submit" value="Submit" className='submit-btn'>Send</button>
             </form>
