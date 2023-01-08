bc83105c3a4fa66c0284da9bdd116a8b79abdecc
michaelc143 <mcorbishley@wisc.edu>
2023-01-08 09:54:55 -0600

    styling fixes for home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -17,6 +17,31 @@ export default function Home() {
             <h4>Pursuing B.S. in Computer Science</h4>
             <p>University of Wisconsin - Madison C/o 2024</p>
           </section>
+
+          <section>
+            <h2 className='coursework'>Relevant Coursework</h2>
+            <ul className='courses'>
+              <li>Computer Engineering & Memory Management</li>
+              <li>Introduction To Algorithms</li>
+              <li>Linear Algebra</li>
+              <li>Discrete Math</li>
+              <li>Introduction To Aritificial Intelligence</li>
+              <li>Computer Graphics</li>
+              <li>Java Programming (I - III)</li>
+            </ul>
+          </section>
+          
+          <section>
+            <h2 className='exp-header'>Experience / Projects</h2>
+            <h3 className='entegral-header'>Entegral - Software Engineer Intern (Summer 2023)</h3>
+            <h5 className='entegral-desc'>Incoming Software Engineer Intern for Full Stack Position at Entegral</h5>
+            <br />
+            <h3 className='webDev-header'>WebDev UW (2020 - Current)</h3>
+            <h5 className='webDev-desc'>UW-Madison student organization focused on web development</h5>
+            <p className='webDev-summary'>WebHacks 2021: Tasked with creating a web application using React. <br />
+              Group App Idea: Bingo game to teach Computer Science to users. <br />
+              Assisted with creating bingo questions related to CS principles as well as Navbar and Leaderboard development and CSS styling</p>
+          </section>
           <section>
             <h2 className='langs-header'>Skills / Languages</h2>
             <table className='langs'>
@@ -32,16 +57,7 @@ export default function Home() {
                 <td>Python, C</td>
               </tr>
             </table>
-          </section>
-          <section>
-            <h2 className='exp-header'>Experience / Projects</h2>
-            <h3 className='entegral-header'>Entegral - Software Engineer Intern (Summer '23)</h3>
-            <h5 className='entegral-desc'>Incoming Software Engineer Intern for Full Stack Position at Entegral</h5>
-            <h3 className='webDev-header'>WebDev UW (2020 - Current)</h3>
-            <h5 className='webDev-desc'>UW-Madison student organization focused on web development</h5>
-            <p className='webDev-summary'>WebHacks 2021: Tasked with creating a web application using React. <br />
-              Group App Idea: Bingo game to teach Computer Science to users. <br />
-              Assisted with creating bingo questions related to CS principles as well as Navbar and Leaderboard development and CSS styling</p>
+            <br />
           </section>
         </main>
       </div>
